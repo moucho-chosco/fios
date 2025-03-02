a786d3a4a5556d8c79da04c647139a08be73de67
moucho-chosco <dsoutopazo3@gmail.com>
2025-03-02 04:39:59 +0100

    proba a ver

---
+++ public/scripts/biblioteca.js
@@ -0,0 +1,126 @@
+function seleccionarLibro(elemento) {
+    // Eliminar a clase 'seleccionado' de todos os libros
+    document.querySelectorAll('.libro').forEach(libro => {
+      libro.classList.remove('seleccionado');
+    });
+  
+    // Engadir a clase 'seleccionado' ao libro clicado
+    elemento.classList.add('seleccionado');
+  
+    // Obter os elementos do banner
+    const fondoBannerEl = document.querySelector('.banner-biblioteca .fondo-banner');
+    const tituloBannerEl = document.querySelector('.banner-biblioteca .titulo-banner');
+    const fondoBannerElWebp = document.querySelector('.banner-biblioteca .fondo-banner-webp');
+    const tituloBannerElWebp = document.querySelector('.banner-biblioteca .titulo-banner-webp');
+    const textoBannerEl  = document.querySelector('.banner-biblioteca .descricion-banner');
+    const bannerBibliotecaEl = document.querySelector('.banner-biblioteca');
+    const botonObraEl = document.querySelector('#boton-obra');
+    const botonItinerarioEl = document.querySelector('#boton-itinerario');
+  
+    // Recoller os valores dos atributos data- do elemento
+    const fondoUrl   = elemento.dataset.fondoBanner;   // corresponde a data-fondo-banner
+    const tituloUrl  = elemento.dataset.tituloBanner;  // corresponde a data-titulo-banner
+    const texto      = elemento.dataset.textoBanner;   // corresponde a data-texto-banner
+    const corBanner  = elemento.dataset.corBanner;      // data-cor-banner
+    const corTexto   = elemento.dataset.corTexto;      // data-cor-texto
+    const marxeTexto = elemento.dataset.marxeTexto;    
+    const tipo       = elemento.dataset.tipo;    
+    const corFondoBoton = elemento.dataset.corFondoBoton;
+    const corTextoBoton = elemento.dataset.corTextoBoton;
+  
+    // Actualizar o banner se os datos existen
+    if (fondoUrl && tituloUrl) {
+
+      const imgFondoWebp  = new Image();
+      const imgTituloWebp = new Image();
+      imgFondoWebp.src  = fondoUrl.replace(/\.(jpg|jpeg|png|gif)$/i, '.webp');
+      imgTituloWebp.src = tituloUrl.replace(/\.(jpg|jpeg|png|gif)$/i, '.webp');
+      let fondoCargado = false;
+      let tituloCargado = false;
+  
+      function verificarCambio() {
+        if (fondoCargado && tituloCargado) {
+          fondoBannerElWebp.srcset  =  fondoUrl.replace(/\.(jpg|jpeg|png|gif)$/i, '.webp');
+          tituloBannerElWebp.srcset =  tituloUrl.replace(/\.(jpg|jpeg|png|gif)$/i, '.webp');
+          fondoBannerEl.src  = fondoUrl;
+          tituloBannerEl.src = tituloUrl;
+          if (texto) {
+            textoBannerEl.textContent = texto;
+          }
+          if (corBanner) {
+              bannerBibliotecaEl.style.backgroundColor = corBanner;
+          }
+          if (corTexto) {
+              textoBannerEl.style.color = corTexto;
+          }
+          if (marxeTexto) {
+              textoBannerEl.style.marginTop = marxeTexto;
+          }
+          if(tipo=="obra"){
+            botonObraEl.style.display = "inline-block";
+            botonItinerarioEl.style.display = "none";
+            botonObraEl.style.backgroundColor = "black";
+            botonObraEl.style.color = "white";
+            if(corFondoBoton)
+              botonObraEl.style.backgroundColor = corFondoBoton;
+            if(corTextoBoton)
+              botonObraEl.style.color = corTextoBoton;
+          } else if (tipo=="itinerario") {
+            botonObraEl.style.display = "none";
+            botonItinerarioEl.style.display = "inline-block";
+            botonItinerarioEl.style.backgroundColor = "black";
+            botonItinerarioEl.style.color = "white";
+            if(corFondoBoton)
+              botonItinerarioEl.style.backgroundColor = corFondoBoton;
+            if(corTextoBoton)
+              botonItinerarioEl.style.color = corTextoBoton;
+          }else{
+            botonObraEl.style.display = "none";
+            botonItinerarioEl.style.display = "none";
+          }
+        }
+      }
+
+      imgFondoWebp.onload = () => {
+          fondoCargado = true;
+          verificarCambio();
+      };
+
+      imgTituloWebp.onload = () => {
+          tituloCargado = true;
+          verificarCambio();
+      };
+    } 
+  }
+  
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const carousels = document.querySelectorAll(".carousel-container");
+    const botonObraEl = document.querySelector('#boton-obra');
+    const botonItinerarioEl = document.querySelector('#boton-itinerario');
+    botonObraEl.style.display = "none";
+    botonItinerarioEl.style.display = "none";
+
+    carousels.forEach((carousel) => {
+        const track = carousel.querySelector(".carousel-track");
+        const prevButton = carousel.querySelector(".carousel-prev");
+        const nextButton = carousel.querySelector(".carousel-next");
+        const bookWidth = carousel.querySelector(".libro").offsetWidth + 15; // Ancho do libro + gap
+
+        let scrollPosition = 0;
+
+        nextButton.addEventListener("click", function () {
+            if (scrollPosition < track.scrollWidth - track.clientWidth) {
+                scrollPosition += bookWidth * 2; // Avanza 2 libros cada vez
+                track.style.transform = `translateX(-${scrollPosition}px)`;
+            }
+        });
+
+        prevButton.addEventListener("click", function () {
+            if (scrollPosition > 0) {
+                scrollPosition -= bookWidth * 2;
+                track.style.transform = `translateX(-${scrollPosition}px)`;
+            }
+        });
+    });
+});
---
+++ public/scripts/functions.js
@@ -0,0 +1,90 @@
+// Función para obter o valor dun parámetro na URL
+function getUrlParameter(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    const sidebarContainer = document.querySelector('.sidebar-container');
+    const bannerBiblioteca = document.querySelector('.banner-biblioteca');
+    const toggleButton = document.querySelector('.toggle-button');
+    const mode = getUrlParameter('sidebar-mode');  // Recuperar o parámetro da URL
+
+    // Desactivamos a transición para evitar animacións no primeiro renderizado
+    sidebarContainer.style.transition = 'none';
+
+    // Aplica inmediatamente o modo correcto antes de que a páxina se renderice
+    if (mode === 'reduced') {
+        sidebarContainer.classList.add('reduced-mode');
+        if(bannerBiblioteca)
+            bannerBiblioteca.classList.add('reduced-mode');
+        toggleButton.innerHTML = '<img src="/images/elementos/frecha_atras_negra_reves.svg" alt="Frecha atrás" width="16" height="16">';  // Frecha á dereita
+        updateSidebarLinksForMode('reduced');
+    } else {
+        sidebarContainer.classList.add('full-mode');
+        if(bannerBiblioteca)
+            bannerBiblioteca.classList.add('full-mode');
+        toggleButton.innerHTML = '<img src="/images/elementos/frecha_atras_negra.svg" alt="Frecha atrás" width="16" height="16">';  // Frecha á esquerda
+        updateSidebarLinksForMode('full');
+    }
+
+    // Usamos un pequeno delay para iniciar as transicións, evitando o parpadeo
+    setTimeout(() => {
+        sidebarContainer.style.transition = 'width 0.3s ease';  // Activamos transición suave
+    }, 70);  // Pequeno delay (50ms) para asegurar que a clase xa estea aplicada
+
+    // Aquí tamén podemos mostrar a páxina agora que o JavaScript está preparado
+    document.body.style.visibility = 'visible';  // Se estaba oculto ao principio
+});
+
+// Función para alternar entre os modos e actualizar a URL
+function toggleSidebar() {
+    const sidebarContainer = document.querySelector('.sidebar-container');
+    const bannerBiblioteca = document.querySelector('.banner-biblioteca');
+    const toggleButton = document.querySelector('.toggle-button');
+    const langSwitcher = document.querySelector('#langSwitcher');
+
+    // Alternar entre os modos
+    sidebarContainer.classList.toggle('reduced-mode');
+    sidebarContainer.classList.toggle('full-mode');
+    if(bannerBiblioteca){
+        bannerBiblioteca.classList.toggle('reduced-mode');
+        bannerBiblioteca.classList.toggle('full-mode');
+    }
+        
+
+    // Se está no modo reducido, engadir o parámetro na URL
+    if (sidebarContainer.classList.contains('reduced-mode')) {
+        toggleButton.innerHTML = '<img src="/images/elementos/frecha_atras_negra_reves.svg" alt="Frecha atrás" width="16" height="16">'  ;  // Frecha á dereita no modo reducido
+        langSwitcher.style.opacity = 0;  // Ocultar o selector de idioma
+        history.pushState(null, null, '?sidebar-mode=reduced');
+        updateSidebarLinksForMode('reduced');
+    } else {
+        toggleButton.innerHTML = '<img src="/images/elementos/frecha_atras_negra.svg" alt="Frecha atrás" width="16" height="16">';  // Frecha á esquerda no modo completo
+        langSwitcher.style.opacity = 1;  // Mostrar o selector de idioma
+        history.pushState(null, null, '?sidebar-mode=full');
+        updateSidebarLinksForMode('full');
+    }
+}
+
+// Función para actualizar os enlaces da barra lateral con base no parámetro sidebar-mode
+function updateSidebarLinksForMode(mode) {
+    const sidebarLinks = document.querySelectorAll('.sidebar a'); // Selecciona todos os enlaces na barra lateral
+    const langSwitcher = document.querySelector('#langSwitcher');
+
+    sidebarLinks.forEach(link => {
+        let url = new URL(link.href); // Obtemos a URL do enlace
+
+        // Se o modo é 'reduced', engadimos o parámetro de URL
+        if (mode === 'reduced') {
+            langSwitcher.style.display = 'none';  // Ocultar o selector de idioma
+            url.searchParams.set('sidebar-mode', 'reduced'); // Cambia o parámetro a 'reduced'
+        } else {
+            langSwitcher.style.display = 'block';  // Mostrar o selector de idioma
+            url.searchParams.delete('sidebar-mode'); // Elimina o parámetro se non é 'reduced'
+        }
+
+        // Actualiza o enlace con a nova URL
+        link.href = url.toString();
+    });
+}
